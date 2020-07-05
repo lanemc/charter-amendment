dde77a11f3c29e76cd4132054f2a8ea5b7bf2ed6
unknown <lanemcunningham@gmail.com>
2020-07-04 19:51:30 -0500

    Updates

---
+++ precache-manifest.7a5f82d9a7aab6af86a8990ce4655d7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd759dd6cabd700a4229493d262f71f",
+    "url": "/charter-amendment/index.html"
+  },
+  {
+    "revision": "2a92e302c120a5535904",
+    "url": "/charter-amendment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e48c3e1355ea979186b7",
+    "url": "/charter-amendment/static/js/2.3b974b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/charter-amendment/static/js/2.3b974b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a92e302c120a5535904",
+    "url": "/charter-amendment/static/js/main.4df47810.chunk.js"
+  },
+  {
+    "revision": "d7e35b441272fffc5d9d",
+    "url": "/charter-amendment/static/js/runtime-main.0398313d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/charter-amendment/static/media/logo.5d5d9eef.svg"
+  }
+]);
