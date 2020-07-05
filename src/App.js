fdfbe1c701d9a36baec3faa89113cd32f0140784
unknown <lanemcunningham@gmail.com>
2020-07-05 13:32:59 -0500

    amendment timeline

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core'
+import AmendmentFaq from './components/AmendmentFaq'
+import AmendmentTimeline from './components/AmendmentTimeline'
 
-function App() {
+const useStyles = makeStyles({
+  root: {
+    background: '#41B3A3',
+  },
+})
+
+const App = () => {
+  const classes = useStyles()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      {/* <AmendmentFaq /> */}
+      <AmendmentTimeline />
     </div>
   );
 }
--- src/components/AmendmentTimeline.js
+++ src/components/AmendmentTimeline.js
@@ -1,102 +1,127 @@
 import React from 'react'
+import { makeStyles, Typography } from '@material-ui/core'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
+import FeedbackIcon from '@material-ui/icons/Feedback'
+import ThumbUpIcon from '@material-ui/icons/ThumbUp'
+import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver'
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople'
+import ThumbsUpDownIcon from '@material-ui/icons/ThumbsUpDown'
+import HowToVoteIcon from '@material-ui/icons/HowToVote'
+import BallotIcon from '@material-ui/icons/Ballot'
+
+const useStyles = makeStyles({
+  heading: {
+    color: '#FFF',
+    marginBottom: '3rem',
+    textAlign: 'center',
+  }, 
+  root: {
+    background: '#41B3A3',
+  },
+})
 
 const AmendmentTimeline = () => {
+  const classes = useStyles()
 
   return (
-    <VerticalTimeline>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-        date="2011 - present"
-        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        //icon={<WorkIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        date="2010 - 2011"
-        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        //icon={<WorkIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">Art Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        date="2008 - 2010"
-        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        //icon={<WorkIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-        <p>
-          User Experience, Visual Design
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        date="2006 - 2008"
-        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        //icon={<WorkIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-        <p>
-          User Experience, Visual Design
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        date="April 2013"
-        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-        //icon={<SchoolIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-        <p>
-          Strategy, Social Media
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        date="November 2012"
-        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-        //icon={<SchoolIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-        <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        date="2002 - 2006"
-        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-        //icon={<SchoolIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-        <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-        <p>
-          Creative Direction, Visual Design
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-        //icon={<StarIcon />}
-      />
-    </VerticalTimeline>
+    <>
+      <Typography variant="h2" className={classes.heading}>[ Charter Amendment Timeline ]</Typography>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: '#8E4982', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #8E4982' }}
+          date="June XX, 2020"
+          iconStyle={{ background: '#fff', color: '#1C4E47' }}
+          icon={<FeedbackIcon />}
+        >
+          <h3 className="vertical-timeline-element-subtitle">Amendment authors provide notice of intent to propose a Charter amendment at the next City Council meeting</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: '#8E4982', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #8E4982' }}
+          date="June XX, 2020"
+          iconStyle={{ background: '#1C4E47', color: '#fff' }}
+          icon={<ThumbUpIcon />}
+        >
+          <h3 className="vertical-timeline-element-subtitle">
+            Council unanimously approves proposal to amend the City Charter; proposal is moved to Charter Commission for consideration to be put on the November 2020 ballot
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: '#8E4982', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #8E4982' }}        
+          date="July 10, 2020"
+          iconStyle={{ background: '#fff', color: '#1C4E47' }}
+          icon={<RecordVoiceOverIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Charter amendment authors testify before the Charter Commission to make the case for why the Charter should be amended</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: '#8E4982', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #8E4982' }}        
+          date="July 11 - August 4, 2020"
+          iconStyle={{ background: '#1C4E47', color: '#fff' }}
+          icon={<EmojiPeopleIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Charter Commission provides opportunities for Public comment on the proposed Charter amendment</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          contentStyle={{ background: '#8E4982', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #8E4982' }}        
+          date="August 5, 2020"
+          iconStyle={{ background: '#fff', color: '#1C4E47' }}
+          icon={<ThumbsUpDownIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">The Charter Commission decides whether or not to add the Charter amendment proposal to the November 2020 ballot for a Public vote</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          contentStyle={{ background: '#8E4982', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #8E4982' }}        
+          date="August 6, 2020"
+          iconStyle={{ background: '#1C4E47', color: '#fff' }}
+          icon={<BallotIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">The Charter Commission's provides their decision/recommendation to the City Council</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          contentStyle={{ background: '#8E4982', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #8E4982' }}        
+          date="August XX"
+          iconStyle={{ background: '#fff', color: '#1C4E47' }}
+          icon={<ThumbsUpDownIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">The City Council decides whether or not to follow the recommendation of the Charter Commission (which may or may not recommend to move the amendment to ballot); If the City Council approves the amendment to move it to ballot, the proposal goes to the Mayor for final approval</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          contentStyle={{ background: '#8E4982', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #8E4982' }}        
+          date="August 21, 2020"
+          iconStyle={{ background: '#1C4E47', color: '#fff' }}
+          icon={<ThumbsUpDownIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">The Mayor decides by the deadline August 21, 2020 whether or not to move the Charter amendment to ballot</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          contentStyle={{ background: '#8E4982', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #8E4982' }}        
+          date="November 2020"
+          iconStyle={{ background: '#fff', color: '#1C4E47' }}
+          icon={<HowToVoteIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">If the City Council and Mayor approve the Charter amendment to move to ballot, the Public will vote in November 2020 on whether or not to approve the Charter amendment</h3>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </>
   )
 }
 
